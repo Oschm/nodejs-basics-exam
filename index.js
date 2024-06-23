03f854c4f08aa294458af8b75d63e704451d0c0c
siratcha dulyakorn <siratcha.air@gmail.com>
2024-06-23 19:47:12 +0700

    implement list contacts

--- index.js
+++ index.js
@@ -14,11 +14,20 @@ function writeContacts(contacts) {
 // Function to add a new contact
 function addContact(name, phone, email) {
     // TODO: Implement this function
+    //read contact file
+    var contacts = readContacts();
+    // add a new contact 
+    var contact = {"name": "Siratcha Dulyakorn", "phone_number": "+66612954429", "email":"siratcha.air@gmail.com"};
+    contact.push(contacts);
+    var contacts = writeContacts(contact);
 }
 
 // Function to remove a contact by name
 function removeContact(name) {
     // TODO: Implement this function
+     //read contact file
+     var contacts = readContacts();
+
 }
 
 // Function to update a contact by name
@@ -29,6 +38,14 @@ function updateContact(name, newPhone, newEmail) {
 // Function to list all contacts
 function listContacts() {
     // TODO: Implement this function
+    console.log("I'm here ");
+    //read contact file
+    var contacts = readContacts();
+    //loop over contacts
+    for (let i = 0; i < contacts.length; i++) {
+        //log contact
+        console.log("Name: %s, Phone number:%s, Email:%s", contacts[i].name, contacts[i].phone_number, contacts[i].email);
+    }
 }
 
 // Function to view a specific contact by name
@@ -40,6 +57,7 @@ function runProgram(){
 //addContact('Alice', '123-456-7890', 'alice@example.com');
 //addContact('Bob', '098-765-4321', 'bob@example.com');
 listContacts();
+addContact();//addContact('Bob', '098-765-4321', 'bob@example.com');
 //viewContact('Alice');
 //updateContact('Alice', '111-222-3333', 'alice@newdomain.com');
 //viewContact('Alice');
